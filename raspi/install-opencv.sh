587e3e4ee51554a422c6880554d8084ee3377be1
kkshmz <kyeshimizu@gmail.com>
2016-11-15 19:57:30 +0900

    install shells for default and audibles

---
+++ raspi/install-opencv.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# All necessary OpenCV dependencies:
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libx264-dev libv4l-dev
+sudo apt-get -y install libgtk2.0-dev
+sudo apt-get -y install libatlas-base-dev gfortran
--- raspi/install.sh
+++ raspi/install.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
 
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get -y install git
+sudo apt-get -y install zsh
+sudo apt-get -y install vim
+sudo apt-get -y install samba samba-common-bin
+sudo apt-get -y install python-dev
+sudo apt-get -y install build-essential
+sudo apt-get -y install cmake
+sudo apt-get -y install pkg-config
+
+sudo pip install --upgrade pip
