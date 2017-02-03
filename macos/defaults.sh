a10586cfa6f294bca8264b5503abfbe9f311d260
Kye Shimizu <kkshmz@Kyes-MacBook-Pro.local>
2017-02-03 15:26:13 +0900

    update with changes to default settings on boot prior to macbook 2016 refresh ordeal

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -287,7 +287,7 @@ chflags nohidden ~/Library
 ###############################################################################
 
 # Set the icon size of Dock items to 36 pixels
-defaults write com.apple.dock tilesize -int 36
+defaults write com.apple.dock tilesize -int 16
 
 # Change minimize/maximize window effect
 defaults write com.apple.dock mineffect -string "scale"
